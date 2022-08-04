952b66be5cf5d6d7f3ec7c2cb5048619a4dc1c61
Omaru Jawara <omarito.jawara@gmail.com>
2022-08-03 21:34:39 -0400

    Add project files.

---
+++ Services/Results.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAZE_DATA_FEED.Services
+{
+    public  partial class Results
+    {
+        public List<Alert>? alerts { get; set; }
+        public ulong endTimeMillis { get; set; }
+        public ulong startTimeMillis { get; set; }
+        public string? startTime { get; set; }
+        public string? endTime { get; set; }
+        public List<Jam>? jams { get; set; }
+    }
+
+
+    public partial class Root
+    {
+        
+    }
+    public partial class Alert
+    {
+        public string? country { get; set; }
+        public string? city { get; set; }
+        public int reportRating { get; set; }
+        public int confidence { get; set; }
+        public int reliability { get; set; }
+        public string? type { get; set; }
+        public string? uuid { get; set; }
+        public int roadType { get; set; }
+        public int magvar { get; set; }
+        public string? subtype { get; set; }
+        public string? street { get; set; }
+        public string? reportDescription { get; set; }
+        public Location? location { get; set; }
+        public ulong pubMillis { get; set; }
+        public int? nThumbsUp { get; set; }
+    }
+    public partial class Location
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+    }
+    public partial class Jam
+    {
+        public string? country { get; set; }
+        public string? city { get; set; }
+        public int level { get; set; }
+        public List<Line>? line { get; set; }
+        public double speedKMH { get; set; }
+        public int length { get; set; }
+        public string? turnType { get; set; }
+        public string? type { get; set; }
+        public ulong uuid { get; set; }
+        public string? endNode { get; set; }
+        public double speed { get; set; }
+        public List<Segment>? segments { get; set; }
+        public int roadType { get; set; }
+        public int delay { get; set; }
+        public string? street { get; set; }
+        public ulong id { get; set; }
+        public ulong pubMillis { get; set; }
+        public string? blockingAlertUuid { get; set; }
+    }
+
+    public partial class Line
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+    }
+
+
+    public partial class Segment
+    {
+    }
+
+
+}
---
+++ Services/WazeDataFeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace WAZE_DATA_FEED.Services
+{
+    public class WazeDataFeed
+    {
+        
+
+    }
+}
